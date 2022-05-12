00a5bd1f785edc967389f4fed673fff33dcb8069
Madzia <magda.cieszynska1003@gmail.com>
2022-05-12 15:41:28 +0200

    first commit

---
+++ src/main/java/pl/sda/partyorganising/PartyorganisingApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.partyorganising;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyorganisingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartyorganisingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sda/partyorganising/PartyorganisingApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.partyorganising;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyorganisingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
