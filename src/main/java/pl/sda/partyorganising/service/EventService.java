982f89ee5eb5ad14e9f945e0e5b510deb709388f
MikolajMozuch <mozuch.mikolaj@gmail.com>
2022-05-17 17:41:59 +0200

    Added Methods in EventService: getEvents, getEvent
    Addeed Methods(@GetMapping) in EventsController:
    getEvents, getEvent

--- src/main/java/pl/sda/partyorganising/service/EventService.java
+++ src/main/java/pl/sda/partyorganising/service/EventService.java
@@ -8,6 +8,7 @@ import pl.sda.partyorganising.repository.EventRepository;
 import pl.sda.partyorganising.repository.UserRepository;
 import pl.sda.partyorganising.web.controller.CreateEventRequest;
 
+import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
@@ -17,6 +18,14 @@ public class EventService {
 
     private final EventRepository eventRepository;
     private final UserRepository userRepository;
+
+    public List<EventEntity> getEvents(){
+        return eventRepository.findAll();
+    }
+
+    public EventEntity getEvent(UUID uuid){
+        return eventRepository.findById(uuid).orElseThrow();
+    }
 
     public UUID createNewEvent(CreateEventRequest request, UUID organizerId) {
         UserEntity userEntity = userRepository.getById(organizerId);
